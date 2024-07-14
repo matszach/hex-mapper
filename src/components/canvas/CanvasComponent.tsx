a8e199dda4e722eb186390cbbad2a5de179ecb9f
matszach <lkaszubowski@gmail.com>
2024-07-14 19:21:43 +0200

    right click drag

--- src/components/canvas/CanvasComponent.tsx
+++ src/components/canvas/CanvasComponent.tsx
@@ -4,16 +4,20 @@ import HexField from './canvas-elements/HexField';
 import { useWindowSize } from 'usehooks-ts';
 import { gridCoords } from '../../utils/calc.utils';
 import { useZoom } from '../../hooks/use-zoom';
+import Konva from 'konva';
+
+Konva.dragButtons = [2]
 
 export default function CanvasComponent() {
   const size = useWindowSize()
   const [zoom, setZoom] = useZoom({ rate: 1.2 })
   // TODO drag on right click only 
   return (
-    <Stage 
+    <Stage
       width={size.width} height={size.height} 
       onWheel={setZoom} scale={zoom}
-      draggable onContextMenu={e => e.evt.preventDefault()}
+      draggable
+      onContextMenu={e => e.evt.preventDefault()}
     >
       <Layer> 
         {gridCoords(50, 30).map(([x, y]) => <HexField key={`${x}-${y}`} pos={[x, y]} />)}
