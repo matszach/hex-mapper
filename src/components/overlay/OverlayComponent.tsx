ead81d7193df3145049da355e080f19055e99465
matszach <lkaszubowski@gmail.com>
2024-07-18 00:03:54 +0200

    brush type select

--- src/components/overlay/OverlayComponent.tsx
+++ src/components/overlay/OverlayComponent.tsx
@@ -5,6 +5,7 @@ import { AppContext } from '../../app-state/app-state.model';
 import AppSelect from './controls/AppSelect';
 import { Button } from 'react-bootstrap';
 import AppColorPalette from './controls/AppColorPalette';
+import { BrushType } from '../../app-state/brush.model';
 
 export default function OverlayComponent() {
   const { brush, updateBrush, undoHistory, palette, updatePalette } = useContext(AppContext)
@@ -15,19 +16,26 @@ export default function OverlayComponent() {
       </nav>
       <aside className='Overlay__aside' style={{ width: ASIDE_WIDTH, height: `calc(100vh - ${NAV_HEIGHT}px)` }}>
         <div className='Overlay__aside__inset'>
-          <AppSelect
-            className='mb-2' 
-            label='Brush size' 
-            value={brush.size} 
-            options={[1, 3, 5, 7, 9, 11, 13, 15]} 
-            onChange={e => updateBrush({ size: Number(e) })} 
-          />
           <AppColorPalette
             className='mb-2'
             palette={palette}
             onEdit={updatePalette}
             value={brush.color}
             onChange={e => updateBrush({ color: e })}
+          />
+          <AppSelect
+            className='mb-2' 
+            label='Brush type' 
+            value={BrushType[brush.type]} 
+            options={['FILL', 'PATTERN', 'ICON']} 
+            onChange={e => updateBrush({ type: BrushType[e as keyof typeof BrushType] })} 
+          />
+          <AppSelect
+            className='mb-2' 
+            label='Brush size' 
+            value={brush.size} 
+            options={[1, 3, 5, 7, 9, 11, 13, 15]} 
+            onChange={e => updateBrush({ size: Number(e) })} 
           />
           <Button onClick={undoHistory}>Undo</Button>
         </div>
