9285414d717ac4900dd678fe613331b59578216c
matszach <lkaszubowski@gmail.com>
2024-07-18 18:59:57 +0200

    cleanup

--- src/components/canvas/canvas-elements/Icon.tsx
+++ src/components/canvas/canvas-elements/Icon.tsx
@@ -1,9 +1,5 @@
 import { Image as KonvaImage } from "react-konva"
 import { FIELD_SIZE } from "../../../const/sizes"
-import useImage from "use-image"
-import { useEffect, useRef } from "react"
-import { RGB } from "konva/lib/filters/RGB"
-import { toRgb } from "../../../utils/color.util"
 import { useIcon } from "../../../hooks/use-icon"
 
 export interface IconProps {
@@ -21,35 +17,15 @@ export default function Icon({ iconKey, x, y, color }: IconProps) {
 }
 
 function InnerIcon({ iconKey = '', x, y, color = '#000000' }: IconProps) {
-  // const [image] = useImage(`./icons/${iconKey}.png`, 'anonymous')
   const image = useIcon(iconKey, color)
-  // const image = useIcon(iconKey, color)
-  // TODO load image by key and color and the store it, filtering as it goes causes lag - this will also let us avoid using ref
-  // lternative would be runig a script to pregenerate the icons (but that would make editing the colors later impossible)
-  // const ref = useRef();
 
-  // useEffect(() => {
-  //   if (image) {
-  //     // @ts-ignore
-  //     ref.current.cache();
-  //   }
-  // }, [image]);
-
-  // TODO use color
-  if (image/* && ref */) {
-    // const [r, g, b] = toRgb(color)
+  if (image) {
     return <KonvaImage
       x={x - FIELD_SIZE / 2}
       y={y - FIELD_SIZE / 2}
       width={FIELD_SIZE}
       height={FIELD_SIZE}
       image={image}
-      // @ts-ignore
-      // ref={ref}
-      // filters={[RGB]}
-      // red={r}
-      // green={g}
-      // blue={b}
     />
   }
   return <></>
