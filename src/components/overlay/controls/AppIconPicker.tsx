0631711ebbed29a6b683de93946c11ee30fca1ca
matszach <lkaszubowski@gmail.com>
2024-07-19 23:30:33 +0200

    +

--- src/components/overlay/controls/AppIconPicker.tsx
+++ src/components/overlay/controls/AppIconPicker.tsx
@@ -1,4 +1,3 @@
-import { toHsl } from '../../../utils/color.util'
 import AppAsideElementWrapper from './AppAsideElementWrapper'
 import './AppIconPicker.scss'
 
@@ -17,6 +16,7 @@ export default function AppIconPicker({ value, color, onChange, iconKeys }: AppI
         className={`AppIconPicker__icons__icon ${key === value ? 'AppIconPicker__icons__icon--selected' : ''}`}
         onClick={() => onChange(key)}
       >
+        {/* COLORS STILL NEED TO BE FIXED HERE */}
         {color === '#000000' ? (
           <img className='AppIconPicker__icons__icon__image' src={`./icons/${key}.png`} style={{ filter: 'invert(100%)' }}/>
         ) : (
