c4486dd57befd06750bb1dc04889f730b40b9666
matszach <lkaszubowski@gmail.com>
2024-07-20 21:58:21 +0200

    +

--- src/components/overlay/controls/AppNavDropdown.tsx
+++ src/components/overlay/controls/AppNavDropdown.tsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
 import './AppNavDropdown.scss'
+import { useBoolean } from 'usehooks-ts'
 
 export interface AppNavDropdownProps {
   label?: string
-  options: ([any, string] |[any, string, Function])[]
+  options: ([any, string] | [any, string, Function])[]
   onChoice?: (value: any) => void,
   children?: React.ReactNode
 }
 
 export default function AppNavDropdown({ label, options, onChoice, children }: AppNavDropdownProps) {
-  const [open, setOpen] = useState(false)
+  // const [open, setOpen] = useState(false)
+  const { value: open, setTrue, setFalse } = useBoolean(false)
   return (
-    <span className='AppNavDropdown' onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)}>
+    <span className='AppNavDropdown' onMouseEnter={setTrue} onMouseLeave={setFalse}>
       {label}{children}
       <span className={`AppNavDropdown__dropdown ${open ? 'AppNavDropdown__dropdown--open' : ''}`}>
         {options.map(([o, text, fn]) => <div 
@@ -19,7 +21,7 @@ export default function AppNavDropdown({ label, options, onChoice, children }: A
           key={o} onClick={() => {
             onChoice && onChoice(o)
             fn && fn()
-            setOpen(false)
+            setFalse()
           }}
         >{text}</div>)}
       </span>
